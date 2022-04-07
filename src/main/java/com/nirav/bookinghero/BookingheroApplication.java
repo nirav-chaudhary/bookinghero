79f58cb23428a4e976277d72677d61905236d229
code-hunterx <30567030+code-hunterx@users.noreply.github.com>
2022-04-07 21:44:58 +0530

    initial commit

---
+++ src/main/java/com/nirav/bookinghero/BookingheroApplication.java
@@ -0,0 +1,13 @@
+package com.nirav.bookinghero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nirav/bookinghero/BookingheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirav.bookinghero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
