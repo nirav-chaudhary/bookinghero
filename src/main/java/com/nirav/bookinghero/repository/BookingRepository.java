e408a280c04675144ed336ca819d8d30afa96985
code-hunterx <30567030+code-hunterx@users.noreply.github.com>
2022-04-08 00:07:17 +0530

    added booking repository

---
+++ src/main/java/com/nirav/bookinghero/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.nirav.bookinghero.repository;
+
+import com.nirav.bookinghero.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Long, Booking> {
+}
